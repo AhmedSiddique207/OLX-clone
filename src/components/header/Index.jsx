b217969a5bd1bfae5fcb1fc5859155ee5c9282d1
Ahmed Siddique <ahmed.siddique.dev@gmail.com>
2024-07-24 05:11:24 +0500

    link active and many more

--- src/components/header/Index.jsx
+++ src/components/header/Index.jsx
@@ -1,96 +1,159 @@
-import React,{useState} from "react";
-import { Link } from "react-router-dom";
-import logo from "./images/OLX-logo.png"
-import "./style.css"
-import Nav from "../navbar/nav"
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import logo from "./images/OLX-logo.png";
+import "./style.css";
+import Nav from "../navbar/nav";
 import LoginSignupModal from "./LoginSignupModal";
-
 
 function Index() {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [isLogin, setIsLogin] = useState(true);
-  
+    const location = useLocation();
+
     const openModal = (isLogin) => {
-      setIsLogin(isLogin);
-      setIsModalOpen(true);
+        setIsLogin(isLogin);
+        setIsModalOpen(true);
     };
-  
+
     const closeModal = () => {
-      setIsModalOpen(false);
+        setIsModalOpen(false);
     };
-  
+
     const toggleMode = () => {
-      setIsLogin(!isLogin);
+        setIsLogin(!isLogin);
     };
+
+    const isActive = (path) => {
+        return location.pathname === path;
+    };
+
     return (
+        <>
+            {/* Fixed upper part */}
+            <div className="fixed-upper">
+                <nav class="navbar navbar-expand-lg">
+                    <div class="container-fluid">
+                        <a
+                            class="navbar-brand olx me-5 pe-5"
+                            href="https://olx.com.pk"
+                            target="_blank"
+                        >
+                            <span className="olx">
+                                <img src={logo} alt="OLX Logo" />
+                            </span>
+                        </a>
+                        <button
+                            class="navbar-toggler"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#navbarNav"
+                            aria-controls="navbarNav"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav">
 
-        <div className="main-position">
-            <nav class="navbar navbar-expand-lg ">
-                <div class="container-fluid">
-                    <a class="navbar-brand olx me-5 pe-5" href="https://olx.com.pk" target="_blank">  <span className="olx"><img src={logo} /></span></a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link nav-link-motor me-5 pe-5" href="../adscreen/vehicles">   <i className="fas fa-car "><span className="icon-text ">Motors</span></i></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link nav-link-property me-5 pe-5" href="../adscreen/sale "><i className="fas fa-building"><span className="icon-text property">Property</span></i></a>
-                            </li>
-
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-
-
-            <nav class="navbar secondary-tab ">
-                <div class="container-fluid ">
-
-                    <div class="select-container">
-                        <i class="fas fa-location-dot"></i>
-                        <select name="options" id="options" class="fw-semibold  ">
-                            <option value="option1">Select Location</option>
-                            <option value="option2">Sindh</option>
-                            <option value="option3">Punjab</option>
-                            <option value="option4">Khyber Pakhtunkhwa</option>
-                            <option value="option5">Balochistan</option>
-                            <option value="option6">Gilgit Baltistan</option>
-                        </select>
-                        <i class="fa-solid fa-caret-down"></i>
-                    </div>
+                                <li className="nav-item">
+                                    <Link
+                                        className={`nav-link nav-link-motor me-5 pe-5 ${isActive("/adscreen/vehicles") ? "active" : ""}`}
+                                        to="/adscreen/vehicles"
+                                    >
+                                        <i className="fas fa-car">
+                                            <span className="icon-text">Motors</span>
+                                        </i>
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link
+                                        className={`nav-link nav-link-property me-5 pe-5 ${isActive("/adscreen/sale") ? "active" : ""}`}
+                                        to="/adscreen/sale"
+                                    >
+                                        <i className="fas fa-building">
+                                            <span className="icon-text property">Property</span>
+                                        </i>
+                                    </Link>
+                                </li>
 
 
-                    <form class="d-flex search" role="search">
-                        <input class=" search-bar" type="search" placeholder="Find Cars,Mobile Phones and more..." aria-label="Search" />
-                        <a href="#" className="search-mag"> <button class=" search-icon" type="submit"> <i class="fas fa-magnifying-glass"></i></button></a>
 
-
-                        <div className="login-sell">
-                          
-      <button type="button" className="btn  login-width btn login fw-bold" onClick={() => openModal(true)}>Login</button>
-      <button type="button" className="btn  signup-width btn sell" onClick={() => openModal(false)}><span><i class="fa-solid fa-plus"></i></span>Sell</button>
-      {isModalOpen && (
-        <LoginSignupModal isLogin={isLogin} closeModal={closeModal} toggleMode={toggleMode} />
-      )}
-                       
-                       
+                            </ul>
                         </div>
-                    </form>
-                    <hr />
+                    </div>
+                </nav>
 
+                <nav class="navbar secondary-tab">
+                    <div class="container-fluid">
+                        <div class="select-container">
+                            <i class="fas fa-location-dot"></i>
+                            <select name="options" id="options" class="fw-semibold">
+                                <option value="option1">Select Location</option>
+                                <option value="option2">Sindh</option>
+                                <option value="option3">Punjab</option>
+                                <option value="option4">Khyber Pakhtunkhwa</option>
+                                <option value="option5">Balochistan</option>
+                                <option value="option6">Gilgit Baltistan</option>
+                            </select>
+                            <i class="fa-solid fa-caret-down"></i>
+                        </div>
 
-                </div>
-            </nav>
-            <div className="ms-1 mt-2">
-                <Nav />
+                        <form class="d-flex search" role="search">
+                            <input
+                                class="search-bar"
+                                type="search"
+                                placeholder="Find Cars,Mobile Phones and more..."
+                                aria-label="Search"
+                            />
+                            <a href="#" className="search-mag">
+                                {" "}
+                                <button class="search-icon" type="submit">
+                                    {" "}
+                                    <i class="fas fa-magnifying-glass"></i>
+                                </button>
+                            </a>
+
+                            <div className="login-sell">
+                                <button
+                                    type="button"
+                                    className="btn  login-width btn login fw-bold"
+                                    onClick={() => openModal(true)}
+                                >
+                                    Login
+                                </button>
+                                <button
+                                    type="button"
+                                    className="btn  signup-width btn sell"
+                                    onClick={() => openModal(false)}
+                                >
+                                    <span>
+                                        <i class="fa-solid fa-plus"></i>
+                                    </span>
+                                    Sell
+                                </button>
+                                {isModalOpen && (
+                                    <LoginSignupModal
+                                        isLogin={isLogin}
+                                        closeModal={closeModal}
+                                        toggleMode={toggleMode}
+                                    />
+                                )}
+                            </div>
+                        </form>
+                    </div>
+                </nav>
             </div>
-        </div>
 
+            <div className="main-content ">
+
+                <hr className=" hr-index" />
+                <div className="ms-1 pt-3">
+                    <Nav />
+                </div>
+            </div>
+        </>
     );
-
 }
 
 export default Index;
--- src/components/navbar/nav.jsx
+++ src/components/navbar/nav.jsx
@@ -1,73 +1,300 @@
-import React from 'react'
+// import React from 'react'
 
-//import style
-import "./stylesheet.css"
+// //import style
+// import "./stylesheet.css"
+
+// export default function Nav() {
+//   return (
+//     <>
+//       <nav class="navbar navbar-expand-lg nav-cat">
+//         <div class="container-fluid">
+//           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+//             <span class="navbar-toggler-icon"></span>
+//           </button>
+//           <div class="collapse navbar-collapse" id="navbarNav">
+//             <ul class="navbar-nav">
+
+
+
+//               <div class="dropdown">
+
+//                 <a class="nav-link navi active dropdown-toggle fw-semibold" aria-current="page" data-bs-toggle="dropdown" aria-expanded="false">All Categories</a>
+
+
+//                 <ul class="dropdown-menu">
+//                   <li><a class="dropdown-item" href="../adscreen/mobile">Mobiles Phones</a></li>
+//                   <hr />
+//                   <li><a class="dropdown-item" href="../adscreen/vehicles">Vehicles</a></li><hr />
+//                   <li><a class="dropdown-item" href="../adscreen/sale">Property for Sale</a></li><hr />
+//                   <li><a class="dropdown-item" href="../adscreen/rent">Property for Rent</a></li><hr />
+//                   <li><a class="dropdown-item" href="../adscreen/electronics">Electronics & Home Appliances</a></li><hr />
+//                   <li><a class="dropdown-item" href="../adscreen/bikes">Bikes</a></li><hr />
+//                   <li><a class="dropdown-item" href="../adscreen/business">Business Industrial & Agriculture</a></li><hr />
+//                   <li><a class="dropdown-item" href="../adscreen/services">Services</a></li><hr />
+//                   <li><a class="dropdown-item" href="../adscreen/jobs">Jobs</a></li><hr />
+//                   <li><a class="dropdown-item" href="../adscreen/animals">Animals</a></li><hr />
+//                   <li><a class="dropdown-item" href="../adscreen/furniture">Furniture & Home Decor</a></li><hr />
+//                   <li><a class="dropdown-item" href="../adscreen/books">Books, Sports & Hobbies</a></li><hr />
+//                   <li><a class="dropdown-item" href="../adscreen/kids">Kids</a></li>
+
+
+
+//                 </ul>
+//               </div>
+
+
+
+//               <li class="nav-item">
+//                 <a class="nav-link navi  " aria-current="page" href="../adscreen/mobile">Mobile Phones</a>
+//               </li>
+//               <li class="nav-item">
+//                 <a class="nav-link navi" href="../adscreen/vehicles">Cars</a>
+//               </li>
+//               <li class="nav-item">
+//                 <a class="nav-link navi" href="../adscreen/bikes">Motorcycles</a>
+//               </li>
+//               <li class="nav-item">
+//                 <a class="nav-link navi" href="../adscreen/rent">Houses</a>
+//               </li>
+//               <li class="nav-item">
+//                 <a class="nav-link navi" href="../adscreen/services">Video Audios</a>
+//               </li>
+//               <li class="nav-item">
+//                 <a class="nav-link navi" href="../adscreen/sale">Land & Plots</a>
+//               </li>
+
+//             </ul>
+//           </div>
+//         </div>
+//       </nav>
+//     </>
+//   )
+// }
+
+
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import "./stylesheet.css";
 
 export default function Nav() {
   return (
     <>
-      <nav class="navbar navbar-expand-lg nav-cat">
-        <div class="container-fluid">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg nav-cat">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-
-
-
-              <div class="dropdown">
-
-                <a class="nav-link navi active dropdown-toggle fw-semibold" aria-current="page" data-bs-toggle="dropdown" aria-expanded="false">All Categories</a>
-
-
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="../adscreen/mobile">Mobiles Phones</a></li>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <div className="dropdown">
+                <NavLink
+                  className="nav-link navi active dropdown-toggle fw-semibold"
+                  aria-current="page"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  All Categories
+                </NavLink>
+                <ul className="dropdown-menu">
+                  <li>
+                    <NavLink
+                      className="dropdown-item"
+                      to="../adscreen/mobile"
+                      activeClassName="active"
+                    >
+                      Mobiles Phones
+                    </NavLink>
+                  </li>
                   <hr />
-                  <li><a class="dropdown-item" href="../adscreen/vehicles">Vehicles</a></li><hr />
-                  <li><a class="dropdown-item" href="../adscreen/sale">Property for Sale</a></li><hr />
-                  <li><a class="dropdown-item" href="../adscreen/rent">Property for Rent</a></li><hr />
-                  <li><a class="dropdown-item" href="../adscreen/electronics">Electronics & Home Appliances</a></li><hr />
-                  <li><a class="dropdown-item" href="../adscreen/bikes">Bikes</a></li><hr />
-                  <li><a class="dropdown-item" href="../adscreen/business">Business Industrial & Agriculture</a></li><hr />
-                  <li><a class="dropdown-item" href="../adscreen/services">Services</a></li><hr />
-                  <li><a class="dropdown-item" href="../adscreen/jobs">Jobs</a></li><hr />
-                  <li><a class="dropdown-item" href="../adscreen/animals">Animals</a></li><hr />
-                  <li><a class="dropdown-item" href="../adscreen/furniture">Furniture & Home Decor</a></li><hr />
-                  <li><a class="dropdown-item" href="../adscreen/books">Books, Sports & Hobbies</a></li><hr />
-                  <li><a class="dropdown-item" href="../adscreen/kids">Kids</a></li>
-
-
-
+                  <li>
+                    <NavLink
+                      className="dropdown-item"
+                      to="../adscreen/vehicles"
+                      activeClassName="active"
+                    >
+                      Vehicles
+                    </NavLink>
+                  </li>
+                  <hr />
+                  <li>
+                    <NavLink
+                      className="dropdown-item"
+                      to="../adscreen/sale"
+                      activeClassName="active"
+                    >
+                      Property for Sale
+                    </NavLink>
+                  </li>
+                  <hr />
+                  <li>
+                    <NavLink
+                      className="dropdown-item"
+                      to="../adscreen/rent"
+                      activeClassName="active"
+                    >
+                      Property for Rent
+                    </NavLink>
+                  </li>
+                  <hr />
+                  <li>
+                    <NavLink
+                      className="dropdown-item"
+                      to="../adscreen/electronics"
+                      activeClassName="active"
+                    >
+                      Electronics & Home Appliances
+                    </NavLink>
+                  </li>
+                  <hr />
+                  <li>
+                    <NavLink
+                      className="dropdown-item"
+                      to="../adscreen/bikes"
+                      activeClassName="active"
+                    >
+                      Bikes
+                    </NavLink>
+                  </li>
+                  <hr />
+                  <li>
+                    <NavLink
+                      className="dropdown-item"
+                      to="../adscreen/business"
+                      activeClassName="active"
+                    >
+                      Business Industrial & Agriculture
+                    </NavLink>
+                  </li>
+                  <hr />
+                  <li>
+                    <NavLink
+                      className="dropdown-item"
+                      to="../adscreen/services"
+                      activeClassName="active"
+                    >
+                      Services
+                    </NavLink>
+                  </li>
+                  <hr />
+                  <li>
+                    <NavLink
+                      className="dropdown-item"
+                      to="../adscreen/jobs"
+                      activeClassName="active"
+                    >
+                      Jobs
+                    </NavLink>
+                  </li>
+                  <hr />
+                  <li>
+                    <NavLink
+                      className="dropdown-item"
+                      to="../adscreen/animals"
+                      activeClassName="active"
+                    >
+                      Animals
+                    </NavLink>
+                  </li>
+                  <hr />
+                  <li>
+                    <NavLink
+                      className="dropdown-item"
+                      to="../adscreen/furniture"
+                      activeClassName="active"
+                    >
+                      Furniture & Home Decor
+                    </NavLink>
+                  </li>
+                  <hr />
+                  <li>
+                    <NavLink
+                      className="dropdown-item"
+                      to="../adscreen/books"
+                      activeClassName="active"
+                    >
+                      Books, Sports & Hobbies
+                    </NavLink>
+                  </li>
+                  <hr />
+                  <li>
+                    <NavLink
+                      className="dropdown-item"
+                      to="../adscreen/kids"
+                      activeClassName="active"
+                    >
+                      Kids
+                    </NavLink>
+                  </li>
                 </ul>
               </div>
 
-
-
-              <li class="nav-item">
-                <a class="nav-link navi  " aria-current="page" href="../adscreen/mobile">Mobile Phones</a>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link navi"
+                  aria-current="page"
+                  to="../adscreen/mobile"
+                  activeClassName="active"
+                >
+                  Mobile Phones
+                </NavLink>
               </li>
-              <li class="nav-item">
-                <a class="nav-link navi" href="../adscreen/vehicles">Cars</a>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link navi"
+                  to="../adscreen/vehicles"
+                  activeClassName="active"
+                >
+                  Cars
+                </NavLink>
               </li>
-              <li class="nav-item">
-                <a class="nav-link navi" href="../adscreen/bikes">Motorcycles</a>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link navi"
+                  to="../adscreen/bikes"
+                  activeClassName="active"
+                >
+                  Motorcycles
+                </NavLink>
               </li>
-              <li class="nav-item">
-                <a class="nav-link navi" href="../adscreen/rent">Houses</a>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link navi"
+                  to="../adscreen/rent"
+                  activeClassName="active"
+                >
+                  Houses
+                </NavLink>
               </li>
-              <li class="nav-item">
-                <a class="nav-link navi" href="../adscreen/services">Video Audios</a>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link navi"
+                  to="../adscreen/services"
+                  activeClassName="active"
+                >
+                  Video Audios
+                </NavLink>
               </li>
-              <li class="nav-item">
-                <a class="nav-link navi" href="../adscreen/sale">Land & Plots</a>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link navi"
+                  to="../adscreen/sale"
+                  activeClassName="active"
+                >
+                  Land & Plots
+                </NavLink>
               </li>
-
             </ul>
           </div>
         </div>
       </nav>
     </>
-  )
+  );
 }
-
